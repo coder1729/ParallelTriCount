3e43aa804133e88fed66b654415cb0a2a49dc142
Ishan Chawla <ishanch@users.noreply.github.com>
2017-04-25 16:19:08 -0400

    Create mpi_gen_patric.c

---
+++ mpi_gen_patric.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc,char** argv)
+
+{
+
+FILE* out = fopen("input.txt","w");
+int i;
+int n = atoi(argv[1]);
+for(i=1;i<=n;i+=10)
+{
+	fprintf(out,"%d %d\n",i,i+1);
+	fprintf(out,"%d %d\n",i,i+2);
+	fprintf(out,"%d %d\n",i,i+5);
+	fprintf(out,"%d %d\n",i,i+6);
+	fprintf(out,"%d %d\n",i,i+7);
+	fprintf(out,"%d %d\n",i,i+8);
+	fprintf(out,"%d %d\n",i,i+9);
+
+	fprintf(out,"%d %d\n",i+1,i);
+	fprintf(out,"%d %d\n",i+1,i+2);
+	fprintf(out,"%d %d\n",i+1,i+3);
+	fprintf(out,"%d %d\n",i+1,i+4);
+	fprintf(out,"%d %d\n",i+1,i+7);
+
+	fprintf(out,"%d %d\n",i+2,i);
+	fprintf(out,"%d %d\n",i+2,i+1);
+	fprintf(out,"%d %d\n",i+2,i+3);
+	fprintf(out,"%d %d\n",i+2,i+4);
+	fprintf(out,"%d %d\n",i+2,i+5);
+
+	fprintf(out,"%d %d\n",i+3,i+1);
+	fprintf(out,"%d %d\n",i+3,i+2);
+	fprintf(out,"%d %d\n",i+3,i+4);
+
+
+	fprintf(out,"%d %d\n",i+4,i+1);
+	fprintf(out,"%d %d\n",i+4,i+2);
+	fprintf(out,"%d %d\n",i+4,i+3);
+
+	fprintf(out,"%d %d\n",i+5,i);
+	fprintf(out,"%d %d\n",i+5,i+2);
+	fprintf(out,"%d %d\n",i+5,i+8);
+
+
+	fprintf(out,"%d %d\n",i+6,i);
+	fprintf(out,"%d %d\n",i+6,i+7);
+	fprintf(out,"%d %d\n",i+6,i+9);
+
+	fprintf(out,"%d %d\n",i+7,i);
+	fprintf(out,"%d %d\n",i+7,i+1);
+	fprintf(out,"%d %d\n",i+7,i+6);
+	
+	fprintf(out,"%d %d\n",i+8,i);
+	fprintf(out,"%d %d\n",i+8,i+5);
+	fprintf(out,"%d %d\n",i+8,i+9);
+	
+
+	fprintf(out,"%d %d\n",i+9,i);
+	fprintf(out,"%d %d\n",i+9,i+6);
+	fprintf(out,"%d %d\n",i+9,i+8);
+	
+	
+
+/*
+
+	%d %d %d %d %d %d %d\n",i,i+1,i+2,i+5,i+6,i+7,i+8,i+9);
+	//fprintf(out,"%d %d %d %d %d %d\n",i+1,i,i+2,i+3,i+4,i+7);
+	fprintf(out,"%d %d %d %d %d %d\n",i+2,i,i+1,i+3,i+4,i+5);
+	fprintf(out,"%d %d %d %d\n",i+3,i+1,i+2,i+4);
+	fprintf(out,"%d %d %d %d\n",i+4,i+1,i+2,i+3);
+	fprintf(out,"%d %d %d %d\n",i+5,i,i+2,i+8);
+	fprintf(out,"%d %d %d %d\n",i+6,i,i+7,i+9);
+	fprintf(out,"%d %d %d %d\n",i+7,i,i+1,i+6);
+	fprintf(out,"%d %d %d %d\n",i+8,i,i+5,i+9);
+	fprintf(out,"%d %d %d %d\n",i+9,i,i+6,i+8);
+*/
+}
+return 0;
+}
